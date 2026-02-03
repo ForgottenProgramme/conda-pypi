008d11ea5517742dc5a2da6c33ae2215808fdef8
Mahe Iram Khan <forgotten.programme@gmail.com>
2026-02-03 11:09:27 +0100

    Revert "Merge branch 'package_extractor_plugin_hook' of github.com:ForgottenProgramme/conda-pypi into package_extractor_plugin_hook"
    This reverts commit bf27da653496e2f1f515c8ef538f6f8d9bd1d0f9, reversing
    changes made to f81ab834cbb18786fe84003fa21872d6175bb187.

--- tests/test_plugins.py
+++ tests/test_plugins.py
@@ -1,7 +1,5 @@
-from conda.base.context import context
 from conda.testing.fixtures import CondaCLIFixture, TmpEnvFixture
 from pytest_mock import MockerFixture
-from conda_pypi import plugin
 from conda_pypi.package_extractor import extract_whl
 import pytest
 from pathlib import Path
@@ -17,7 +15,6 @@ CONDA_URL = "https://repo.anaconda.com/pkgs/main/osx-arm64/boltons-25.0.0-py314h
         pytest.param(WHL_HTTP_URL, 1, id=".whl url"),
         pytest.param("{file}", 1, id=".whl file"),
         pytest.param("file:///{file}", 1, id=".whl file url"),
-        pytest.param(CONDA_URL, 0, id=".conda url"),
     ],
 )
 def test_extract_whl_as_conda_called(
@@ -30,9 +27,6 @@ def test_extract_whl_as_conda_called(
     package: str,
     call_count: int,
 ):
-    # Check .whl extractor is registered
-    assert context.plugin_manager.get_package_extractor(".whl")
-
     package = package.format(file=pypi_demo_package_wheel_path)
     with tmp_env() as prefix:
         # mock python installed in prefix
@@ -41,15 +35,14 @@ def test_extract_whl_as_conda_called(
             return_value=("3.10", str(tmp_path)),
         )
 
-        # spy on the wheel extractor function in the plugin module
-        spy = mocker.spy(plugin, "extract_whl_as_conda_pkg")
+        # spy on monkeypatches
+        spy_extract_whl_as_conda_pkg = mocker.spy(extract_whl, "extract_whl_as_conda_pkg")
 
         # install package
-        _, _, err = conda_cli("install", f"--prefix={prefix}", package)
-        assert not err
+        conda_cli("install", f"--prefix={prefix}", package)
 
         # wheel extraction only happens for .whl
-        assert spy.call_count == call_count
+        assert spy_extract_whl_as_conda_pkg.call_count == call_count
 
 
 def test_extract_whl_as_conda_pkg(
